0926ded7ca93c87fef81573fb656a0acb8ef6970
mallikaraju <marri.mallika@gmail.com>
2023-07-15 15:28:10 +0530

    first commit

---
+++ src/main/java/com/maven/spring/web/maven01/SpringWebMaven01Application.java
@@ -0,0 +1,13 @@
+package com.maven.spring.web.maven01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMaven01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMaven01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/spring/web/maven01/SpringWebMaven01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.spring.web.maven01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMaven01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
