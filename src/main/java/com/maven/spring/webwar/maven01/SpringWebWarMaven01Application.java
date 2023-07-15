ec36ae083b9ecfef451bff17676794e43d4c3c20
mallikaraju <marri.mallika@gmail.com>
2023-07-15 18:29:30 +0530

    maven project

---
+++ src/main/java/com/maven/spring/webwar/maven01/SpringWebWarMaven01Application.java
@@ -0,0 +1,13 @@
+package com.maven.spring.webwar.maven01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebWarMaven01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebWarMaven01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/spring/webwar/maven01/SpringWebWarMaven01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.spring.webwar.maven01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebWarMaven01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
